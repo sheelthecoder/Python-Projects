e18422ce768813a1d6f3f6c80e061ffcde3feb4e
Sheel Ashish Shah <120591678+sheelthecoder@users.noreply.github.com>
2026-02-19 22:38:02 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+c=a+b
+print("the addition of",a,"and",b,"is",c)
+c=a-b
+print("the subtraction of",a,"and",b,"is",c)
+c=a*b
+print("the multiplication of",a,"and",b,"is",c)
+c=a/b
+print("the division of",a,"and",b,"is",c)
