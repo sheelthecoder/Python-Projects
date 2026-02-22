b205a1cde1eb5cd318d43fde55b1aba867e05468
Sheel Ashish Shah <120591678+sheelthecoder@users.noreply.github.com>
2026-02-22 22:48:12 +0530

    Update Cube of a number.py

--- Cube of a number.py
+++ Cube of a number.py
@@ -5,12 +5,3 @@ while(i<=10):
     i=num*i*i
 
 print("Cube of number is",i)
-
-#Square of a number
-num=int(input("Enter the number "))
-i=num
-while(i<=10):
-    print(num," * ",num," - ", num*i)
-    i=num*i
-
-print("Square of the number is",i)
