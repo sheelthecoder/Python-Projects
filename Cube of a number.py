528058ff8bcbd5071f1d593ab4d671975cd8b2b8
Sheel Ashish Shah <120591678+sheelthecoder@users.noreply.github.com>
2026-02-22 22:47:22 +0530

    Add files via upload

---
+++ Cube of a number.py
@@ -0,0 +1,16 @@
+num=int(input("Enter the number "))
+i=num
+while(i<=10):
+    print(num," * ",num," * ",num," - ", num*i*i)
+    i=num*i*i
+
+print("Cube of number is",i)
+
+#Square of a number
+num=int(input("Enter the number "))
+i=num
+while(i<=10):
+    print(num," * ",num," - ", num*i)
+    i=num*i
+
+print("Square of the number is",i)
